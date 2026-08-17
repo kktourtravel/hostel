d8b0e39ed2bbdd3ab2e048f99d641ddfa0808f57
kktourtravel <crish2way@gmail.com>
2026-08-17 15:15:14 +0300

    Update roomRoutes.js

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -5,5 +5,7 @@ const roomController = require("../controllers/roomController");
 router.get("/availability", roomController.checkAvailability);
 router.get("/list", roomController.getAllRooms);
 router.get("/:id", roomController.getRoomDetails);
+router.get("/calendar", roomController.getCalendar);
+
 
 module.exports = router;
