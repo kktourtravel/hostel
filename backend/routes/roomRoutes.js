76c0887ecab2bd9c4c330ba89479c96baea34686
kktourtravel <crish2way@gmail.com>
2026-08-18 01:33:41 +0300

    Update roomRoutes.js

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const roomController = require("../controllers/roomController");
 
+// ALWAYS PUT SPECIFIC ROUTES FIRST
 router.get("/availability", roomController.checkAvailability);
 router.get("/list", roomController.getAllRooms);
-router.get("/:id", roomController.getRoomDetails);
 router.get("/calendar", roomController.getCalendar);
 
+// THEN PUT DYNAMIC ROUTES LAST
+router.get("/:id", roomController.getRoomDetails);
 
 module.exports = router;
